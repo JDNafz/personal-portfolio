bd4d4b6d6db6cd08751dbf9a88f0d2b0cd3d3192
JDNafz <nafziger10@gmail.com>
2023-09-24 23:31:27 -0500

    Feelings moved over

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,73 +1,24 @@
 import "./App.css";
-import { HashRouter as Router, Route } from "react-router-dom";
-
-import Feeling from "../redux-feeling/Feeling/Feeling";
-import Understanding from "../redux-feeling/Understanding/Understanding";
-import Support from "../redux-feeling/Support/Support";
-import Comments from "../redux-feeling/Comments/Comments";
-import Review from "../redux-feeling/Review/Review";
-import SubmitSuccess from "../redux-feeling/SubmitSuccess/SubmitSuccess";
 import { Link } from "react-router-dom";
-import Admin from "../Admin/Admin";
-import { Card } from "@mui/material";
 
-function App() {
+import { HashRouter as Router, Route } from "react-router-dom";
+import { Card } from "@mui/material";
+import ReduxFeeling from "../redux-feeling/ReduxFeeling";
+
+export default function App() {
   return (
     <Router>
       <div className="nav">
+        <Link to={"/"}>Home</Link>
         <Link to={"/feedback"}>Feedback App</Link>
         <Link to={"/portfolio"}>Portfolio</Link>
       </div>
+
       <Route path="/portfolio">
         <Card />
-      
       </Route>
+
       <ReduxFeeling />
-
-
-      <Route path="/feedback">
-        <div className="header"></div>
-        <div className="feedbackApp">
-          <header className="feedbackApp-header">
-            <h1 className="feedbackApp-title">Feedback!</h1>
-            <Link to={"/feedback/admin"}>
-              <h4>Admin page</h4>
-            </Link>
-          </header>
-        </div>
-
-        <Route exact path="/feedback">
-          <Feeling />
-        </Route>
-
-        <Route exact path="/feedback/understanding">
-          <Understanding />
-        </Route>
-
-        <Route exact path="/feedback/support">
-          <Support />
-        </Route>
-
-        <Route exact path="/feedback/comments">
-          <Comments />
-        </Route>
-
-        <Route exact path="/feedback/review">
-          <Review />
-        </Route>
-
-        <Route exact path="/feedback/submit-success">
-          <SubmitSuccess />
-        </Route>
-
-        <Route exact path="/feedback/admin">
-          <Admin />
-        </Route>
-    
-      </Route> 
     </Router>
-    
-  );
-}
-
-export default App;
+  ); //end return
+} //end App
