98335dc9243eed85bdfb94931659b197afa12066
JDNafz <nafziger10@gmail.com>
2023-10-01 23:58:04 -0500

    trying to have gitignore work

--- src/components/WelcomPage/WelcomePage.jsx
+++ src/components/WelcomPage/WelcomePage.jsx
@@ -5,16 +5,22 @@ import { Box } from "@mui/material";
 export default function WelcomePage() {
   return (
     <div className="welcomePage">
-      <Box
-        component="img"
-        alt="JD image"
-        src="images/jd-sq.jpg"
-        sx={{ width: 400, borderRadius: 50, margin: 0 }}
-      />
-      <h1 id="jdName">
-        Hi, my <br />
-        name is <b>JD</b>
-      </h1>
+      <div id="picAndName">
+        <Box
+          id="JDImage"
+          component="img"
+          alt="JD image"
+          src="images/jd-sq.jpg"
+          sx={{ width: 400, borderRadius: 50, margin: 0 }}
+        />
+        <h1 id="jdName">
+          Hi, my <br />
+          name is <b>JD</b>
+        </h1>
+      </div>
+      <h4 id="checkOutWork">Checkout some of my work</h4>
+      <div id="line1"></div>
+      <h4 id="scrollLabel">scroll...</h4>
     </div>
   );
 }
