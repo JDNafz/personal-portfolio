ae31ccb9b842dd70c1944cd6a7f4579eacf437e9
JDNafz <nafziger10@gmail.com>
2023-10-01 23:09:50 -0500

    added an images folder and a new picture of myself

--- src/components/WelcomPage/WelcomePage.jsx
+++ src/components/WelcomPage/WelcomePage.jsx
@@ -1,13 +1,15 @@
 import Avatar from "@mui/material/Avatar";
 import "./WelcomePage.css";
+import { Box } from "@mui/material";
 
 export default function WelcomePage() {
   return (
     <div className="welcomePage">
-      <Avatar
+      <Box
+        component="img"
         alt="JD image"
-        src="../../../public/JD.jpg"
-        sx={{ width: 240, height: 240 }}
+        src="images/jd-sq.jpg"
+        sx={{ width: 400, borderRadius: 50, margin: 0 }}
       />
       <h1 id="jdName">
         Hi, my <br />
