400425659faced496dac4c87e8e61a3a66415247
JDNafz <nafziger10@gmail.com>
2023-10-09 01:04:12 -0500

    added extra projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -12,44 +12,35 @@ export default function Portfolio() {
   return (
     <>
       <div id="portfolioContainer">
-      <a href="https://weekend-movie-sagas-jdn-d35d1fb91beb.herokuapp.com/#/">
+      <a href="https://github.com/JDNafz/JustChess">
           <Card className="portfolioCard">
-            <CardActionArea onClick={() => handleClick("movie")}>
+            <CardActionArea >
               <CardMedia
                 component="img"
-                image="images/movie-demo.gif"
+                image="images/Chess_Demo_10.1.23.gif"
                 alt={`Image of Portfolio example.`}
               />
             </CardActionArea>
-          </Card>
-        </a>
-        
-        <a href="https://weekend-movie-sagas-jdn-d35d1fb91beb.herokuapp.com/#/">
-          <Card className="portfolioCard">
-            <CardActionArea onClick={() => handleClick("movie")}>
-              <CardMedia
-                component="img"
-                image="images/movie-demo.gif"
-                alt={`Image of Portfolio example.`}
-              />
-            </CardActionArea>
+            <span>*Currently in Progress* due date is 10/23/23</span>
           </Card>
         </a>
 
-        <a href="https://to-do-list-jdn-d1ad334ecfe5.herokuapp.com/">
+        <a href="https://weekend-movie-sagas-jdn-d35d1fb91beb.herokuapp.com/#/">
           <Card className="portfolioCard">
-            <CardActionArea onClick={() => handleClick("to-do")}>
+            <CardActionArea>
               <CardMedia
                 component="img"
-                image="images/todo-list-app.gif"
+                image="images/movie-demo.gif"
                 alt={`Image of Portfolio example.`}
               />
             </CardActionArea>
+            <span> 2 Day Sprint completed on October 1st, 2023</span>
           </Card>
         </a>
-        <a href="https://server-side-calculator-jdn-6b027b5f3ade.herokuapp.com/">
+
+        <a href="https://react-gallery-jd-144f294c43b1.herokuapp.com/">
           <Card className="portfolioCard">
-            <CardActionArea onClick={() => handleClick("calculator")}>
+            <CardActionArea>
               <CardMedia
                 component="img"
                 height="330"
@@ -57,6 +48,35 @@ export default function Portfolio() {
                 alt={`Image of Portfolio example.`}
               />
             </CardActionArea>
+            <span> 2 Day Sprint completed on September 17th, 2023</span>
+
+          </Card>
+        </a>
+
+        <a href="https://to-do-list-jdn-d1ad334ecfe5.herokuapp.com/">
+          <Card className="portfolioCard">
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                image="images/todo-list-app.gif"
+                alt={`Image of Portfolio example.`}
+              />
+            </CardActionArea>
+            <span> 2 Day Sprint completed on September 3rd, 2023</span>
+          </Card>
+        </a>
+        <a href="https://server-side-calculator-jdn-6b027b5f3ade.herokuapp.com/">
+          <Card className="portfolioCard">
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="330"
+                image="images/EmployeeCalcProject.png"
+                alt={`Image of Portfolio example.`}
+              />
+            </CardActionArea>
+            <span> 2 Day Sprint completed on August 24th, 2023</span>
+
           </Card>
         </a>
       </div>
