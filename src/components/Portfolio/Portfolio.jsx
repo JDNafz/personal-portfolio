8508df0c79a0037b40db01c77c93e700c6036da8
JDNafz <nafziger10@gmail.com>
2023-10-09 00:08:13 -0500

    added chess demo image

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -12,30 +12,41 @@ export default function Portfolio() {
   return (
     <>
       <div id="portfolioContainer">
-    <a href="https://weekend-movie-sagas-jdn-d35d1fb91beb.herokuapp.com/#/">
-        <Card className="portfolioCard">
-          <CardActionArea onClick={() => handleClick("movie")}>
-            <CardMedia
-              component="img"
-              image="images/movie-demo.gif"
-              alt={`Image of Portfolio example.`}
-            />
-          </CardActionArea>
-        </Card>
-
+      <a href="https://weekend-movie-sagas-jdn-d35d1fb91beb.herokuapp.com/#/">
+          <Card className="portfolioCard">
+            <CardActionArea onClick={() => handleClick("movie")}>
+              <CardMedia
+                component="img"
+                image="images/movie-demo.gif"
+                alt={`Image of Portfolio example.`}
+              />
+            </CardActionArea>
+          </Card>
+        </a>
+        
+        <a href="https://weekend-movie-sagas-jdn-d35d1fb91beb.herokuapp.com/#/">
+          <Card className="portfolioCard">
+            <CardActionArea onClick={() => handleClick("movie")}>
+              <CardMedia
+                component="img"
+                image="images/movie-demo.gif"
+                alt={`Image of Portfolio example.`}
+              />
+            </CardActionArea>
+          </Card>
         </a>
 
         <a href="https://to-do-list-jdn-d1ad334ecfe5.herokuapp.com/">
-        <Card className="portfolioCard">
-          <CardActionArea onClick={() => handleClick("to-do")}>
-            <CardMedia
-              component="img"
-              image="images/todo-list-app.gif"
-              alt={`Image of Portfolio example.`}
-            />
-          </CardActionArea>
-        </Card>
-              </a>
+          <Card className="portfolioCard">
+            <CardActionArea onClick={() => handleClick("to-do")}>
+              <CardMedia
+                component="img"
+                image="images/todo-list-app.gif"
+                alt={`Image of Portfolio example.`}
+              />
+            </CardActionArea>
+          </Card>
+        </a>
         <a href="https://server-side-calculator-jdn-6b027b5f3ade.herokuapp.com/">
           <Card className="portfolioCard">
             <CardActionArea onClick={() => handleClick("calculator")}>
@@ -48,7 +59,6 @@ export default function Portfolio() {
             </CardActionArea>
           </Card>
         </a>
-
       </div>
       <div className="fillBottom" />
     </>
