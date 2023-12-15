230034b8fa145e82738091cd44b1de3e4112fc48
JDNafz <nafziger10@gmail.com>
2023-12-15 11:46:02 -0600

    remove RRD from app.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 
-import { HashRouter as Router, Route } from "react-router-dom";
 import Header from "../Header/Header";
 import WelcomePage from "../WelcomePage/WelcomePage";
 import Portfolio from "../Portfolio/Portfolio";
